2287a386349394826758c44dcecd00b938b9fac7
Jagankumar E <53823168+jagankumar-egov@users.noreply.github.com>
2026-03-31 11:24:58 +0530

    Add new Plug configurations for abdm and care_scribe

--- plug_config.py
+++ plug_config.py
@@ -1,6 +1,20 @@
 from plugs.manager import PlugManager
-from plugs.plug import Plug  # noqa: F401
+from plugs.plug import Plug
 
-plugs = []
+plugs = [
+    
+    Plug(
+        name="abdm",
+        package_name="git+https://github.com/care-ecosystem/care_abdm.git",
+        version="@develop",
+        configs={},
+    ),
+    Plug(
+        name="care_scribe",
+        package_name="git+https://github.com/care-ecosystem/care_scribe.git",
+        version="@master",
+        configs={},
+    ),
+]
 
 manager = PlugManager(plugs)
