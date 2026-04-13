fe711c9610a8b10c9baf65fca19795267ec1991d
pranavrutagi-egov <pranav.rutagi@egovernments.org>
2026-04-13 10:54:34 +0530

    Integrated Plugin into CARE

--- plug_config.py
+++ plug_config.py
@@ -1,6 +1,23 @@
 from plugs.manager import PlugManager
 from plugs.plug import Plug  # noqa: F401
 
-plugs = []
+care_digit_integration_plug = Plug(
+    name="care_digit_integration",
+    package_name="git+https://github.com/care-ecosystem/care_digit_integration.git",
+    version="@main",
+    configs={
+        "HOST": "",
+        "DIGIT_TOKEN_ENDPOINT": "",
+        "PGR_CREATE_ENDPOINT": "",
+        "USERNAME": "",
+        "PASSWORD": "",
+        "USER_TYPE": "",
+        "GRANT_TYPE": "",
+    },
+)
+
+plugs = [
+    care_digit_integration_plug,
+]
 
 manager = PlugManager(plugs)
