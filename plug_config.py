595075a57534886d72603e52f38328f6e0a39bbf
Jagankumar E <53823168+jagankumar-egov@users.noreply.github.com>
2026-06-02 18:44:40 +0530

    Add care_eaushadhi plugin configuration

--- plug_config.py
+++ plug_config.py
@@ -21,12 +21,12 @@ plugs = [
         package_name="git+https://github.com/care-ecosystem/care_radiology.git",
         version="@updates",
         configs={
-        "CARE_RADIOLOGY_DCM4CHEE_DICOMWEB_BASEURL": os.getenv(
-            "CARE_RADIOLOGY_DCM4CHEE_DICOMWEB_BASEURL"
-        ),
-        "CARE_RADIOLOGY_WEBHOOK_SECRET": os.getenv(
-            "CARE_RADIOLOGY_WEBHOOK_SECRET"
-        )
+            "CARE_RADIOLOGY_DCM4CHEE_DICOMWEB_BASEURL": os.getenv(
+                "CARE_RADIOLOGY_DCM4CHEE_DICOMWEB_BASEURL"
+            ),
+            "CARE_RADIOLOGY_WEBHOOK_SECRET": os.getenv(
+                "CARE_RADIOLOGY_WEBHOOK_SECRET"
+            )
         }
     ),
      Plug(
@@ -34,16 +34,16 @@ plugs = [
         package_name="git+https://github.com/care-ecosystem/care_digit_integration.git",
         version="@main",
         configs={
-        "HOST": "https://sandbox.digit.org",
-        "DIGIT_TOKEN_ENDPOINT": "/user/oauth/token",
-        "DIGIT_HEADER_AUTH_TOKEN": "ZWdvdi11c2VyLWNsaWVudDo=",
-        "PGR_CREATE_ENDPOINT": "/pgr-services/v2/request/_create",
-        "PGR_FETCH_ENDPOINT": "/pgr-services/v2/request/_search",
-        "FILESTORE_UPLOAD_ENDPOINT": "/filestore/v1/files",
-        "GRANT_TYPE": "password",
-        "USERNAME": "9965664222",
-        "PASSWORD": "123456",
-        "USER_TYPE": "citizen"
+            "HOST": "https://sandbox.digit.org",
+            "DIGIT_TOKEN_ENDPOINT": "/user/oauth/token",
+            "DIGIT_HEADER_AUTH_TOKEN": "ZWdvdi11c2VyLWNsaWVudDo=",
+            "PGR_CREATE_ENDPOINT": "/pgr-services/v2/request/_create",
+            "PGR_FETCH_ENDPOINT": "/pgr-services/v2/request/_search",
+            "FILESTORE_UPLOAD_ENDPOINT": "/filestore/v1/files",
+            "GRANT_TYPE": "password",
+            "USERNAME": "9965664222",
+            "PASSWORD": "123456",
+            "USER_TYPE": "citizen"
         }
     ),
     Plug(
@@ -51,6 +51,19 @@ plugs = [
         package_name="git+https://github.com/10bedicu/care_scribe.git",
         version="@master",
         configs={},
+    ),
+    Plug(
+        name="care_eaushadhi",
+        package_name="git+https://github.com/care-ecosystem/care_eaushadhi.git",
+        version="@main",
+        configs={
+            "EAUSHADHI_API_ENDPOINT": os.getenv(
+            "EAUSHADHI_API_ENDPOINT"
+            ),
+            "EAUSHADHI_API_SECRET_KEY": os.getenv(
+                "EAUSHADHI_API_SECRET_KEY"
+            )
+        }
     ),
     Plug(
         name="care_communication",
