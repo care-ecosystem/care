93855b1b7bd66c7e869135044c84778c2a402ea1
Jagankumar E <53823168+jagankumar-egov@users.noreply.github.com>
2026-06-04 18:41:33 +0530

    Add care_radiology plug configuration

--- plug_config.py
+++ plug_config.py
@@ -23,6 +23,7 @@ plugs = [
         version="@main",
         configs={},
     ),
+    
     Plug(
         name="care_radiology",
         package_name="git+https://github.com/care-ecosystem/care_radiology.git",
